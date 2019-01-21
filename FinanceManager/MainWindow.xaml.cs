5e15abe4b7aa92511b1653b913d1811d58c80a02
eli hebdon <elihebdon@gmail.com>
2019-01-20 22:32:43 -0700

    setting up

--- FinanceManager/MainWindow.xaml.cs
+++ FinanceManager/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
 
 namespace FinanceManager
 {
@@ -23,6 +24,58 @@ namespace FinanceManager
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = ConfigurationManager.ConnectionStrings["FinanceManager.Properties.Settings.FinanceDBConnectionString"].ConnectionString;
+        }
+
+        private void AccountList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_Account_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Account_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DateList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_Date_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Remove_Date_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TransactionList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Add_Transaction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Delete_Transaction_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Update_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
--- FinanceManager/Properties/Settings.Designer.cs
+++ FinanceManager/Properties/Settings.Designer.cs
@@ -8,22 +8,29 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FinanceManager.Properties
-{
-
-
+namespace FinanceManager.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=ELI;Initial Catalog=FinanceDB;Persist Security Info=True;User ID=sa;P" +
+            "assword=Sochi2016!")]
+        public string FinanceDBConnectionString {
+            get {
+                return ((string)(this["FinanceDBConnectionString"]));
             }
         }
     }
